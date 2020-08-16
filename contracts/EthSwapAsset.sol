44da23942b1313e0ef9db9e3c92a3c5c3a78546a
jowenshaw <jowenshaw@qq.com>
2020-08-16 23:02:42 +0800

    update dcrm owner

--- contracts/EthSwapAsset.sol
+++ contracts/EthSwapAsset.sol
@@ -563,18 +563,38 @@ pragma solidity ^0.5.0;
 
 
 contract EthSwapAsset is ERC20, ERC20Detailed {
+    event LogChangeDCRMOwner(address indexed oldOwner, address indexed newOwner, uint indexed effectiveHeight);
     event LogSwapin(bytes32 indexed txhash, address indexed account, uint amount);
     event LogSwapout(address indexed account, address indexed bindaddr, uint amount);
 
-    address public owner;
+    address private _oldOwner;
+    address private _newOwner;
+    uint256 private _newOwnerEffectiveHeight;
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "only owner");
+        require(msg.sender == owner(), "only owner");
         _;
     }
 
     constructor() public ERC20Detailed("SMPC Ethereum", "mETH", 18) {
-        owner = msg.sender;
+        _newOwner = msg.sender;
+        _newOwnerEffectiveHeight = block.number;
+    }
+
+    function owner() public view returns (address) {
+        if (block.number >= _newOwnerEffectiveHeight) {
+            return _newOwner;
+        }
+        return _oldOwner;
+    }
+
+    function changeDCRMOwner(address newOwner) public onlyOwner returns (bool) {
+        require(newOwner != address(0), "new owner is the zero address");
+        _oldOwner = owner();
+        _newOwner = newOwner;
+        _newOwnerEffectiveHeight = block.number + 13300;
+        emit LogChangeDCRMOwner(_oldOwner, _newOwner, _newOwnerEffectiveHeight);
+        return true;
     }
 
     function Swapin(bytes32 txhash, address account, uint256 amount) public onlyOwner returns (bool) {
